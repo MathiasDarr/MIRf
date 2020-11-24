018cf89e09f624fa08755d57151ec9e5f8e45325
mddarr <mddarr@gmail.com>
2020-11-24 04:46:15 -0800

    create a dynamodb table for files uploaded by the user

--- transcription-api/app.py
+++ transcription-api/app.py
@@ -8,49 +8,63 @@ app = Flask(__name__)
 app.config.from_object("config")
 
 s3 = boto3.client("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+dynamo = boto3.resource('dynamodb')  # , endpoint_url='http://localhost:8000')
+table = dynamo.Table('UserUploads')
 
-def parse_email(user_email):
-    split = user_email.split("@")
-    host = split[1].split('.')[0]
-    return '{}_{}'.format(split[0],host)
 
-@app.route("/upload/<user>", methods=["POST"])
-def upload_file(user):
+def user_upload_file(user, file):
     """
-        These attributes are also available
-        file.filename               # The actual name of the file
-        file.content_type
-        file.content_length
-        file.mimetype
-    """
+    This function uploads the uploaded file to s3 and creates an item in the database with the uploaded files url
 
+    :param user: email address of authenticated user
+    :param file: uploaded file
+    :return:
+    """
     user_directory = parse_email(user)
-
-    # A
-    if "user_file" not in request.files:
-        return "No user_file key in request.files"
-
-    # B
-    file = request.files["user_file"]
-    if file:
-        file.filename = secure_filename(file.filename)
-    if file.filename == "":
-        return "Please select a file"
     try:
         s3.upload_fileobj(
             file,
             S3_BUCKET,
-            '{}/{}'.format(user_directory,file.filename),
+            '{}/{}'.format(user_directory, file.filename),
             ExtraArgs={
                 "ContentType": file.content_type
             }
         )
-        return "{}{}".format(S3_LOCATION, file.filename)
+
+        filepath = "{}{}/{}".format(S3_LOCATION,user_directory, file.filename)
+
+        table.put_item(
+            Item={
+                'user': user,
+                'fileurl': filepath,
+            }
+        )
+        return filepath
 
     except Exception as e:
         # This is a catch all exception, edit this part to fit your needs.
         print("Something Happened: ", e)
         return e
+
+
+def parse_email(user_email):
+    split = user_email.split("@")
+    host = split[1].split('.')[0]
+    return '{}_{}'.format(split[0], host)
+
+
+@app.route("/upload/<user>", methods=["POST"])
+def upload_file(user):
+    if "user_file" not in request.files:
+        return "No user_file key in request.files"
+
+    file = request.files["user_file"]
+    if file:
+        file.filename = secure_filename(file.filename)
+    if file.filename == "":
+        return "Please select a file"
+
+    return user_upload_file(user, file)
 
 
 if __name__ == "__main__":
---
+++ transcription-api/data_model/create_user_upload_table.py
@@ -0,0 +1,40 @@
+"""
+This script creates the dynamoDB transcriptions table
+
+"""
+# !/usr/bin/env python3
+
+import boto3
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url='http://localhost:8000')
+try:
+    resp = dynamodb.create_table(
+        AttributeDefinitions=[
+            {
+                "AttributeName": "user",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "fileurl",
+                "AttributeType": "S"
+            },
+        ],
+        TableName="UserUploads",
+        KeySchema=[
+            {
+                "AttributeName": "user",
+                "KeyType": "HASH"
+            },
+            {
+                "AttributeName": "fileurl",
+                "KeyType": "RANGE"
+            }
+        ],
+        ProvisionedThroughput={
+            "ReadCapacityUnits": 1,
+            "WriteCapacityUnits": 1
+        },
+    )
+
+except Exception as e:
+    print(e)
+
