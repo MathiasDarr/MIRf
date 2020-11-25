b3f388d1f9efd5f28e0f02ce2ab3f13fcfeedbe8
mddarr <mddarr@gmail.com>
2020-11-24 17:29:43 -0800

    Start working on the audio recording list

--- mir-vue/src/components/audio/AudioRecordingsList.vue
+++ mir-vue/src/components/audio/AudioRecordingsList.vue
@@ -7,6 +7,32 @@
       </v-card-title>
     </v-card-text>
     {{ getUserUploads }}
+
+
+    <v-card flat>
+            <v-list two-line>
+            <v-list-item-group >
+                <template v-for="(item, index) in getUserUploads">
+                  <v-list-item :key="item.filename">
+                
+                    <template>
+                        {{ index }}
+                      <!-- <v-list-item-content @click="selectUserUpload(item.filename)">
+                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                        <v-list-item-subtitle class="text--primary">Guitar </v-list-item-subtitle>
+                      </v-list-item-content> -->
+                    </template>
+                  </v-list-item>
+                  <!-- <v-divider
+                    v-if="index + 1 < items.length"
+                    :key="index">
+                  </v-divider> -->
+                
+                </template>
+              </v-list-item-group>
+            </v-list>  
+    </v-card>
+
   </v-card>
  </div>   
 </template>
